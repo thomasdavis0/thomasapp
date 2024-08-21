218c905374e40074d5f3d43e7986bcee7afb7c5d
thomasdavis0 <thomas.davis.00001@gmail.com>
2024-08-21 21:06:47 +0300

    Update app.js
    test

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const { App } = require('@slack/bolt');
-
-// Loads environment variables from a .env file
 require('dotenv').config();
+
+const { App } = require('@slack/bolt');
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-// Listens to the slash command '/first-bolt'
-app.command('/first-bolt', async ({ command, ack, respond }) => {
-  // Acknowledge the command request
-  await ack();
-
-  try {
-    // Respond to the command with a message
-    await respond(`Hello, <@${command.user_id}>! You just ran the /first-bolt command.`);
-  } catch (error) {
-    console.error('Error responding to the command:', error);
-  }
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+    console.log('hello');
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  });
 });
 
 (async () => {
-  await app.start(process.env.PORT || 10000);
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
   console.log('⚡️ Bolt app is running!');
 })();
